4ad64ff2d12691d36a2b21d8ef6de95757b890ee
martinmicunda <martinmicunda@hotmail.com>
2015-06-13 17:49:53 +0100

    add partnes module

--- index.js
+++ index.js
@@ -1,55 +1,44 @@
+/**
+ * @author    Martin Micunda {@link http://martinmicunda.com}
+ * @copyright Copyright (c) 2015, Martin Micunda
+ * @license	  GPL-3.0
+ */
 'use strict';
 
-var pkg     = require('./package.json');
-var logger  = require('mm-node-logger')(module);
+/**
+ * Module dependencies.
+ */
+var logger    = require('mm-node-logger')(module);
+var pkg       = require('./package.json');
+var koa       = require('./lib/config/koa');
+var config    = require('./lib/config/config');
+var couchbase = require('./lib/config/couchbase');
 
-//import koa from 'koa';
-var koa = require('koa');
-let app = koa();
+const serverBanner = `
+*********************************************************************************************
+*
+* ${pkg.description}
+* @version ${pkg.version}
+* @author ${pkg.author.name}
+* @copyright 2014-${new Date().getFullYear()} ${pkg.author.name}
+* @license ${pkg.license}
+*
+* ${'App started on port:'.blue} ${config.server.port} ${'- with environment:'.blue} ${config.environment}
+*
+*********************************************************************************************`;
 
+// Initialize couchbase
+couchbase.init(function startServer() {
+    // Initialize koa
+    const app = koa.init();
 
+    // Start up the server on the port specified in the config after we connected to couchbase
+    app.listen(config.server.port, function() {
+        logger.info(serverBanner);
+    });
 
-var couchbase = require('couchbase');
-var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
-var bucket = cluster.openBucket('default');
-
-//bucket.upsert('testdoc', {name:'Frank'}, function(err, result) {
-//    if (err) throw err;
-//
-//    bucket.get('testdoc', function(err, result) {
-//        if (err) throw err;
-//
-//        console.log(result.value);
-//        // {name: Frank}
-//    });
-//});
-
-app.use(function *(){
-    let test = 'Hello Word';
-    this.body = test;
-     //var arr = ['a', 'e', 'i', 'o', 'u'];
-    //arr.forEach(vowel => {
-    //    console.log(vowel);
-    //});
-});
-//expressApp.route(apiVersion + '/package')
-//    .get(function(req, res) {
-//        res.status(200).send(require('../package'));
-//    });
-
-app.listen(3000, function () {
-    var serverBanner = ['',
-    '*************************************' + ' KOA SERVER '.yellow + '********************************************',
-    '*',
-    `* ${pkg.description}`,
-        `* @version ${pkg.version}`,
-        `* @author ${pkg.author.name}`,
-        `* @copyright 2014-${new Date().getFullYear()} ${pkg.author.name}`,
-        `* @license ${pkg.license.type}, ${pkg.license.url}`,
-        '*',
-    '*' + ` App started on port: ${process.env.APP_PORT} - with environment: ${process.env.APP_ENV}`.blue,
-    '*',
-    '*************************************************************************************************',
-    ''].join('\n');
-    logger.info(serverBanner);
+    /**
+     * Expose `Application`.
+     */
+    module.exports = app;
 });
